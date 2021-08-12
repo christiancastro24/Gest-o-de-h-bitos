c010a097fa5217ae7dd0bc540bf08860aa63cfb0
Gustavo P. Nunes <gustavo-pradogt@hotmail.com>
2021-08-12 17:01:31 -0300

    reverting changes

--- src/Components/CreateGroup/index.js
+++ src/Components/CreateGroup/index.js
@@ -1,24 +1,6 @@
 import CreateGroupContainer from "./style";
-import {TextField} from "@material-ui/core";
-import { makeStyles } from '@material-ui/core/styles';
 
 const CreateGroup = () => {
-    
-    const useStyles = makeStyles((theme) => ({
-        inputs: {
-            backgroundColor: 'white',
-            width: "100%",
-            height: "44px",
-            maxWidth: "24rem",
-            outline: "none",
-            borderRadius: "4px",
-            textAlign: "center",
-            margin: "8px auto",
-            // boxShadow: "5px 6px 10px -2px black",
-        },
-    }));
-
-    const classes = useStyles();
     
     return (
         <>  
